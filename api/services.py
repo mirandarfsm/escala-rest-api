e8c56233f56e5c804c2343c0ffaec22af027c305
vagrant <vagrant@precise32.(none)>
2016-06-11 14:53:27 -0300

    fix escala semanal

--- api/services.py
+++ api/services.py
@@ -60,7 +60,6 @@ class ServicoDiarioService(object):
             else:
                 tipo = TipoServico.PRETO    
             servico = Servico(data=d,tipo=tipo)
-            print servico
             is_servico_exists(servico,escala)
             servicos.append(servico)
             if servico.tipo == TipoServico.PRETO:
@@ -116,59 +115,82 @@ class ServicoDiarioService(object):
         return lista_usuario_escala
 
 class ServicoSemanalService(object):
+    def iso_to_gregorian(self,iso_year, iso_week, iso_day):
+        "Gregorian calendar date for the given ISO year, week and day"
+        fifth_jan = datetime.date(iso_year, 1, 5)
+        _, fifth_jan_week, fifth_jan_day = fifth_jan.isocalendar()
+        return fifth_jan + datetime.timedelta(days=iso_day-fifth_jan_day, weeks=iso_week-fifth_jan_week)
     
     def gerar_lista_servico(self,year,month,escala):
-        roxas = [roxa.isocalendar()[1] for roxa in escala.roxas]
+        roxas = [roxa.isocalendar()[1] for roxa in escala.get_datas_roxas()]
+        datas_vermelhas = escala.get_datas_vermelhas()
+        datas_roxas = escala.get_datas_roxas()
         servicos = []
         dates = calendar.monthcalendar(year, month)
+        week = [calendar.MONDAY,calendar.TUESDAY,calendar.WEDNESDAY,calendar.THURSDAY,calendar.FRIDAY]
         for d in dates:
             tipo = TipoServico.PRETO
-            day = d[calendar.MONDAY]
-            if day != 0:
-                date = datetime.date(year,month,day)
-                if date.isocalendar()[1] in roxas:
-                    tipo = TipoServico.ROXA
-                servicos.append(Servico(data=date,tipo=tipo,escala_id=escala.id))
+            for week_day in week:    
+                day = d[week_day]
+                if day != 0:
+                    date = datetime.date(year,month,day)
+                    if date in datas_vermelhas or date in datas_roxas:
+                        continue
+                    else:
+                        if date.isocalendar()[1] in roxas:
+                            tipo = TipoServico.ROXA
+                        servico = Servico(data=date,tipo=tipo)
+                        is_servico_exists(servico,escala)
+                        servicos.append(servico)
+                        break
+                else:
+                    break
         return servicos
 
-    def is_descanso(self,militar,date):
-        for servico in militar.servicos:
-            descanso_start = servico.data.isocalendar()[1]-1
-            descanso_end = servico.data.isocalendar()[1]+1
-            if descanso_start <= date.isocalendar()[1] <= descanso_end:
-                return True
+    def is_descanso(self,usuario_escala,data):
+        data_iso = data.isocalendar()
+        data_inicio = self.iso_to_gregorian(*(data_iso[0],data_iso[1]-1,7))
+        data_fim = self.iso_to_gregorian(*(data_iso[0],data_iso[1]+1,2))
+        servico = usuario_escala.servicos.filter(Servico.data.between(data_inicio,data_fim)).first()
+        if servico:
+            return True
         return False
 
-    def is_afastado(self,militar,date):
-        for afastamento in militar.afastamentos:
-            if afastamento.data_inicio.isocalendar()[1] <= date.isocalendar()[1] <= afastamento.data_fim.isocalendar()[1]:
+    def is_afastado(self,usuario,date):
+        afastamento = usuario.afastamentos.filter(Afastamento.ativo == True) \
+                            .filter(Afastamento.data_revisao is not None) \
+                            .filter(Afastamento.data_inicio <= date, Afastamento.data_fim >= date).first()
+        if afastamento:
                 return True     
         return False
     
-    def get_next_military(self,militares,servico):
-        for i in range(len(militares)):
-            militar = militares[0]
-            if not self.is_afastado(militar,servico.data):
-                if not self.is_descanso(militar,servico.data):
-                    militares.pop(0)
-                    militares.append(militar)
-                    return militar
-            militares[0] = militares[i+1]
-            militares[i+1] = militar
-        raise Exception("Nao existe militares para serem escalados na data: "+ str(servico.data))
+    def get_next_military(self,lista_usuario_escala,servico):
+        for i in range(len(lista_usuario_escala)):
+            usuario_escala = lista_usuario_escala[0]
+            if not self.is_afastado(usuario_escala.usuario,servico.data):
+                if not self.is_descanso(usuario_escala,servico.data):
+                    lista_usuario_escala.pop(0)
+                    lista_usuario_escala.append(usuario_escala)
+                    return usuario_escala
+            lista_usuario_escala[0] = lista_usuario_escala[i+1]
+            lista_usuario_escala[i+1] = usuario_escala
+        raise Exception("Nao existem militares para serem escalados na data: "+ str(servico.data))
 
     def gerar_lista_militares_escalados(self,escala):
-        militares = escala.usuarios.all()
+        lista_usuario_escala = escala.usuarios.all()
+        if not lista_usuario_escala:
+            raise Exception("Nao existem militares cadastrado na escala")
         hash = {}
-        hash[TipoServico.PRETO] = sorted(militares,key=Usuario.by_preta_key)
-        hash[TipoServico.ROXA] = sorted(militares,key=Usuario.by_roxa_key)
+        hash[TipoServico.PRETO] = sorted(lista_usuario_escala,key=UsuarioEscala.by_preta_key)
+        hash[TipoServico.ROXA] = sorted(lista_usuario_escala,key=UsuarioEscala.by_roxa_key)
         date = get_next_month(datetime.date.today())
         servicos = self.gerar_lista_servico(date.year,date.month,escala)
         for servico in servicos:
-            militar = self.get_next_military(hash[servico.tipo],servico)
-            militar.servicos.append(servico)
+            usuario_escala = self.get_next_military(hash[servico.tipo],servico)
+            servico.id_usuario_escala = usuario_escala.id
+            db.session.add(servico)
             db.session.commit()
-        return militares
+        return lista_usuario_escala
 
 
 def get_next_military_old(milicos,servico):
