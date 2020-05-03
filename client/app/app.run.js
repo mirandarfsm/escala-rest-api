be988885b1f3d5df95829bb32597ee6e4ded390a
mirandarfsm <harripobre@gmail.com>
2020-05-03 02:17:25 -0300

    added token in a localStorage

--- client/app/app.run.js
+++ client/app/app.run.js
@@ -1,26 +1,27 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('Escalante')
-    .run(run);
-  
-  run.$inject = ['$rootScope','$location','$http','autenticacaoService'];
+    angular
+        .module('Escalante')
+        .run(run);
 
-  function run($rootScope, $location, $http,autenticacaoService) {
-    $rootScope.$on('$locationChangeStart',locationChangeStart); 
-    
-    function locationChangeStart(event, next, current) {
-      if (autenticacaoService.isLogged()) {
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa(autenticacaoService.getToken()+':');
-      }
-      $rootScope.isAuthenticated = autenticacaoService.isLogged();
-      $rootScope.isAdmin = autenticacaoService.isAdmin();
-      $rootScope.isEscalante = autenticacaoService.isEscalante();
-      if ($location.path() !== '/login' && !autenticacaoService.isLogged()) {
-        $location.path('/login');
-      };
+    run.$inject = ['$rootScope', '$location', '$http', 'autenticacaoService'];
+
+    function run($rootScope, $location, $http, autenticacaoService) {
+        $rootScope.$on('$locationChangeStart', locationChangeStart);
+
+        function locationChangeStart(event, next, current) {
+            if (autenticacaoService.isLogged()) {
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa(autenticacaoService.getToken() + ':');
+            }
+            $rootScope.usuario = autenticacaoService.getUsuario();
+            $rootScope.isAuthenticated = autenticacaoService.isLogged();
+            $rootScope.isAdmin = autenticacaoService.isAdmin();
+            $rootScope.isEscalante = autenticacaoService.isEscalante();
+            if ($location.path() !== '/login' && !autenticacaoService.isLogged()) {
+                $location.path('/login');
+            }
+        }
     }
-  }
 
 })();
--- client/app/services/autenticacao.service.js
+++ client/app/services/autenticacao.service.js
@@ -1,81 +1,82 @@
-(function(){
-	'use strict';
+(function() {
+    'use strict';
 
-	angular
-		.module('Escalante')
-		.factory('autenticacaoService',autenticacaoService);
+    angular
+        .module('Escalante')
+        .factory('autenticacaoService', autenticacaoService);
 
-	autenticacaoService.$inject = ['Restangular','$http','$rootScope','$timeout'];
+    autenticacaoService.$inject = ['Restangular', '$http', '$rootScope', '$timeout'];
 
-	function autenticacaoService(Restangular,$http,$rootScope,$timeout){
+    function autenticacaoService(Restangular, $http, $rootScope, $timeout) {
 
-		var usuario = undefined;
+        var usuario = angular.fromJson(localStorage.user);
 
-		var service = {
-			get: get,
-			login: login,
-			getUsuario: getUsuario,
-			getToken: getToken,
-			logout: logout,
-			isLogged: isLogged,
-			isAdmin: isAdmin,
-			isEscalante: isEscalante
+        var service = {
+            get: get,
+            login: login,
+            getUsuario: getUsuario,
+            getToken: getToken,
+            logout: logout,
+            isLogged: isLogged,
+            isAdmin: isAdmin,
+            isEscalante: isEscalante
 
-		};
+        };
 
-		function login(username, password) {
-	    	var headers = {Authorization: "Basic " + btoa(username + ":" + password)};
-	    	return $http.get('/auth/request-token', {headers : headers}).success(function(data){
-		        usuario = data.usuario;
-		        usuario.token = data.token;
-		        $rootScope.usuario = usuario;
-		        $timeout(logout, 3600000);
-		        return usuario;
-		      });
-	  	}
+        function login(username, password) {
+            var headers = { Authorization: "Basic " + btoa(username + ":" + password) };
+            return $http.get('/auth/request-token', { headers: headers }).success(function(data) {
+                usuario = data.usuario;
+                usuario.token = data.token;
+                localStorage.user = angular.toJson(usuario);
+                $timeout(logout, 3600000);
+                return usuario;
+            });
+        }
 
-		function get(){
-			return Restangular.service('usuarios').one('me');
-		}
+        function get() {
+            return Restangular.service('usuarios').one('me');
+        }
 
-		function getUsuario(){
-	    	return usuario;
-	  	}
+        function getUsuario() {
+            return usuario;
+        }
 
-		function getToken(){
-			return usuario.token;
-		}
+        function getToken() {
+            return usuario.token;
+        }
 
-		function logout(){
-			usuario = undefined;
-			$rootScope.usuario = undefined;
-		}
+        function logout() {
+            usuario = undefined;
+            $rootScope.usuario = undefined;
+            localStorage.removeItem('user');
+        }
 
-		function isLogged(){
-			return !!usuario;
-		}
+        function isLogged() {
+            return !!usuario;
+        }
 
-		function isAdmin(){
-			if(service.isLogged()){
-			  return checkPerfil(usuario.perfis,0);
-			}
-			return false;
-		}
+        function isAdmin() {
+            if (service.isLogged()) {
+                return checkPerfil(usuario.perfis, 0);
+            }
+            return false;
+        }
 
-		function isEscalante(){
-			if(service.isLogged()){
-			  return checkPerfil(usuario.perfis,1);
-			}
-			return false;
-		}
+        function isEscalante() {
+            if (service.isLogged()) {
+                return checkPerfil(usuario.perfis, 1);
+            }
+            return false;
+        }
 
-		function checkPerfil(arr, val) {
-			return arr.some(function(arrVal) {
-				return val === arrVal;
-			});
-		}
+        function checkPerfil(arr, val) {
+            return arr.some(function(arrVal) {
+                return val === arrVal;
+            });
+        }
 
-		return service;
-	}
+        return service;
+    }
 
 })();
