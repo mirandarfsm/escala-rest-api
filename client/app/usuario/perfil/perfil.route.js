802d64311c49c07d072c951275c0baafef332bfa
mirandarfsm <harripobre@gmail.com>
2020-05-04 19:33:23 -0300

    change password implemented

--- client/app/usuario/perfil/perfil.route.js
+++ client/app/usuario/perfil/perfil.route.js
@@ -1,18 +1,26 @@
-(function(){
-	'use strict';
+(function() {
+    'use strict';
 
-	angular
-		.module('Escalante')
-		.config(config);
+    angular
+        .module('Escalante')
+        .config(config);
 
-  config.$inject = ['$routeProvider'];
+    config.$inject = ['$routeProvider'];
 
-	function config($routeProvider){
-		$routeProvider
-		.when('/perfil-usuario', {
-		    templateUrl: 'app/usuario/perfil/perfil.html',
-		    controller: 'PerfilUsuarioController',
-		    controllerAs: 'vm'
-		});
-	}
+    function config($routeProvider) {
+        $routeProvider
+            .when('/perfil-usuario', {
+                templateUrl: 'app/usuario/perfil/perfil.html',
+                controller: 'PerfilUsuarioController',
+                controllerAs: 'vm',
+                resolve: {
+                    passwordChangeOne: passwordChangeOne
+                }
+            });
+    }
+
+    function passwordChangeOne(autenticacaoService) {
+        return autenticacaoService.get().one('change-password');
+    }
+
 })();
